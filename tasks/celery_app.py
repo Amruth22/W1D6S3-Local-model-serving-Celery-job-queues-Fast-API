b348fb400feffcdf6cfce381f13559899d79f567
Amruth22 <89969774+Amruth22@users.noreply.github.com>
2025-08-23 19:15:20 +0530

    Add broker transport options to Celery configuration

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -53,22 +53,28 @@ os.makedirs(CELERY_RESULTS_DIR, exist_ok=True)
 celery_app = Celery('rag_tasks')
 
 # Configure Celery
-celery_app.conf.update(
-    broker_url=CELERY_BROKER_URL,
-    result_backend=CELERY_RESULT_BACKEND,
-    task_serializer=CELERY_TASK_SERIALIZER,
-    result_serializer=CELERY_RESULT_SERIALIZER,
-    accept_content=CELERY_ACCEPT_CONTENT,
-    timezone=CELERY_TIMEZONE,
-    enable_utc=CELERY_ENABLE_UTC,
-    task_time_limit=TASK_TIMEOUT,
-    task_soft_time_limit=TASK_TIMEOUT - 30,
-    worker_prefetch_multiplier=1,
-    task_acks_late=True,
-    worker_disable_rate_limits=True,
-    task_ignore_result=False,
-    result_expires=3600,  # Results expire after 1 hour
-)
+config_dict = {
+    'broker_url': CELERY_BROKER_URL,
+    'result_backend': CELERY_RESULT_BACKEND,
+    'task_serializer': CELERY_TASK_SERIALIZER,
+    'result_serializer': CELERY_RESULT_SERIALIZER,
+    'accept_content': CELERY_ACCEPT_CONTENT,
+    'timezone': CELERY_TIMEZONE,
+    'enable_utc': CELERY_ENABLE_UTC,
+    'task_time_limit': TASK_TIMEOUT,
+    'task_soft_time_limit': TASK_TIMEOUT - 30,
+    'worker_prefetch_multiplier': 1,
+    'task_acks_late': True,
+    'worker_disable_rate_limits': True,
+    'task_ignore_result': False,
+    'result_expires': 3600,  # Results expire after 1 hour
+}
+
+# Add broker transport options if using filesystem broker
+if 'CELERY_BROKER_TRANSPORT_OPTIONS' in globals():
+    config_dict['broker_transport_options'] = CELERY_BROKER_TRANSPORT_OPTIONS
+
+celery_app.conf.update(config_dict)
 
 # Auto-discover tasks
 celery_app.autodiscover_tasks(['tasks'])
